4807498a09011d8af8173cf26f9a0ad2143d96e9
Pietro <victor_pietro@hotmail.com>
2019-06-29 07:57:26 -0300

    first commit

---
+++ weatherforecast/src/main/java/br/com/hbsis/weatherforecast/WeatherforecastConfiguration.java
@@ -0,0 +1,8 @@
+package br.com.hbsis.weatherforecast;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+
+public class WeatherforecastConfiguration {
+}
